abd73771e44b3699c56f7f5a1b1fb8a37b29e203
Haneen Alnuwaysir <haneenalnuwaysir@gmail.com>
2024-03-04 20:08:03 +0300

    edit some forms

--- src/app/recipes/recipe-start/recipe-start.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { RecipeStartComponent } from './recipe-start.component'
-
-describe('RecipeStartComponent', () => {
-  it('should mount', () => {
-    cy.mount(RecipeStartComponent)
-  })
-})
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,17 +1,21 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component,  OnDestroy,  OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent implements OnInit {
+export class RecipesListComponent implements OnInit, OnDestroy {
   recipes!: Recipe[] ;
-    
+  subscribtion !: Subscription;
+
+
   constructor( private recipeService: RecipeService, private router: Router, private route: ActivatedRoute){}
+
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
   }
@@ -19,6 +23,12 @@ export class RecipesListComponent implements OnInit {
   onNewRecipe(){
     this.router.navigate(['new'], {relativeTo: this.route});
 
+  }
+
+  ngOnDestroy(): void {
+    // throw new Error('Method not implemented.');
+    // to save ur memory
+    this.subscribtion.unsubscribe();
   }
 
 }
